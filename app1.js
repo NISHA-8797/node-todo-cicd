0d48f613532015f3f957a715946487de6f1f81b6
NISHA-8797 <61613669+NISHA-8797@users.noreply.github.com>
2025-07-29 15:38:35 +0530

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,2 +1,2 @@
-echo "console.log('Change from genx branch');" > app1.js
-git commit -am "Update app1.js in genx"
+//echo "console.log('Change from genx branch');" > app1.js
+//git commit -am "Update app1.js in genx"
